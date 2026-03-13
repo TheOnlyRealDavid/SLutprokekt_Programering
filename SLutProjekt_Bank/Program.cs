12a84aa6b31b74a9cf980ae2abb69b1a3ffc9ee1
David Bodeang <david.bodeang@gmail.com>
2026-03-13 11:24:38 +0100

    Har arbetat på banUpplevelse metoden

--- SLutProjekt_Bank/Program.cs
+++ SLutProjekt_Bank/Program.cs
@@ -7,9 +7,9 @@
 // Bankupplevelsen
 // Användaren befinner sig i sin digitala bank och får välja vad hen vill göra.
 // Användaren kan välja att visa sitt saldo.
-// Användaren kan välja att sätta in pengar på kontot.
-// Användaren kan välja att ta ut pengar från kontot.
-// Användaren kan välja att investera sina pengar i olika alternativ.
+// Användaren kan välja att vida möjliga uppgraderingar och invensteringar.
+// Användaren kan välja att gå till nästa dag;
+// Användaren kan Välja att Avsluta spelet.
 
 
 // Investeringar
@@ -41,9 +41,119 @@
 
 //====================================================================================
 // START
-//------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------
 
- static void start ()
+int saldo = start(); // här sparas startkapital värdet i int saldo som start metoden retunerar
+BankUpplevelse(saldo); // skickar in saldo värdet i bankupplevesen metoden
+
+ static int start()
 {
+    Console.WriteLine("===== BANK SIMULATOR =====");
+    Console.WriteLine("Målet är att bygga upp din förmögenhet genom att spara och investera pengarna och nå 1 miljon kr.");
+    Console.WriteLine();
+
+    Console.Write("Skriv ditt namn för att skapa ett bankkonto: ");
+    string namn = Console.ReadLine();
+
+    int startkapital = 1000;
+
+    Console.WriteLine($"Välkommen {namn}! Du börjar med {startkapital} kr.");
+
+    Console.ReadLine();
+    Console.Clear();
+    
+    return startkapital; // Jag retunerar startkaptitalet till saldo variabeln vilket gör så jag kan använda den i andra metoder
+}
+
+//===============================================================================================
+// Bankuppleveslen
+//-----------------------------------------------------------------------------------------------
+
+static void BankUpplevelse(int saldo) // Jag behöver hämta saldo variabeln från start metoden
+{
+    bool spela = true;
+
+    while (spela == true)
+    {
+        Console.WriteLine("===== DIN BANK =====");
+        Console.WriteLine("1. Visa saldo");
+        Console.WriteLine("2. Visa Uppgraderingar/Invensteringar");
+        Console.WriteLine("3. Nästa Dag");
+        Console.WriteLine("4. Avsluta");
+        Console.WriteLine("Skriv siffran på vad du vill göra");
+        string BankValen = Console.ReadLine();
+        int.TryParse(BankValen, out int BankValet);
+
+            while(BankValet < 1 || BankValet > 4)
+            {
+                Console.WriteLine("Du måste skriva en siffra på vad du vill göra, 1-4");
+                BankValen = Console.ReadLine();
+                int.TryParse(BankValen, out BankValet);
+                
+            }
+
+
+        if(BankValet == 1)
+        {
+            Console.WriteLine($"Du har ett Saldo på {saldo} kr");
+            Console.ReadLine();
+        }
+
+        else if(BankValet == 2)
+        {
+            Console.WriteLine("===== INVESTERINGAR =====");
+            Console.WriteLine("1. Lemonadstånd (kostar 500 kr, tjänar 50 kr/dag)");
+            Console.WriteLine("2. Aktier (kostar 2000 kr, tjänar 200 kr/dag)");
+            Console.WriteLine("3. Tillbaka");
+
+            string Invensteringsval = Console.ReadLine();
+            int.TryParse(Invensteringsval, out int investeringValet);
+
+            if(investeringValet == 1)
+            {
+                if(saldo >= 500)
+                {
+                    saldo -= 500;
+                    Console.WriteLine("Du köpte ett Lemonadstånd!");
+                    Console.ReadLine();
+                }
+
+                else
+                {
+                    Console.WriteLine("Du har inte tillräckligt med pengar!");
+                    Console.ReadLine();
+                }
+            }
+
+            if(investeringValet == 2)
+            {
+                if(saldo >= 2000)
+                {
+                    saldo -= 2000;
+                    Console.WriteLine("Du köpte ett Aktier!");
+                    Console.ReadLine();
+                }
+                
+                else
+                {
+                    Console.WriteLine("Du har inte tillräckligt med pengar!");
+                    Console.ReadLine();
+                }
+            }
+        }
+
+        else if(BankValet == 3)
+        {
+            Console.WriteLine("Du valde 3");
+            Console.ReadLine();
+        }
+
+        else if(BankValet == 4)
+        {
+            spela = false;
+        }
+    }
     
 }
+
+
